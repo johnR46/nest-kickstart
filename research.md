0c30d43aa99037acf3841a42b71951a16d63e12d
JohnR46 <Johnnote7@gmail.com>
2021-02-02 01:14:22 +0700

    init connect with pg

---
+++ research.md
@@ -0,0 +1,9 @@
+# nest and pg
+
+# Doc
+
+- https://docs.nestjs.com/
+
+# install type orm and pg
+
+- npm install --save @nestjs/typeorm typeorm pg
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: '',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection){
+    
+  }
+}
